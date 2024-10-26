a111b2b0d6cd919f430e589d739ff1af28d539ec
thekey213 <azdilinga@gmail.com>
2024-10-26 23:05:42 +0200

    Setting up deployment env

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import ghPages from 'vite-plugin-gh-pages';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    ghPages(), // Add the GitHub Pages plugin here
+  ],
+  base: '/Talentime/', // Replace 'Talentime' with your repository name
+});
