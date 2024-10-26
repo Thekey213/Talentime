fd0795bef0dd968458b1da8221fdb2e737646333
thekey213 <azdilinga@gmail.com>
2024-10-27 00:36:23 +0200

    Added the rounting

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,12 @@ import Homepage from './components/Homepage';
 import './App.css';
 
 function App() {
-  return (
-    <Router>
-      <div>
-        <h1>My Simple App</h1>
 
-        {/* Define Routes */}
-        <Routes>
-          <Route path="/" element={<Homepage />} />
+  return (
+    <Router basename="/Talentime">
+      <Routes>
+        <Route path="/" element={<Homepage />} />
         </Routes>
-      </div>
     </Router>
   );
 }
