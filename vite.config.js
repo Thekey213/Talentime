88a48bc7882abf10e7fc2e40e5c9e084f281d71d
thekey213 <azdilinga@gmail.com>
2024-10-26 23:23:53 +0200

    Second setup

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,8 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react-swc';
-import ghPages from 'vite-plugin-gh-pages';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    ghPages(), // Add the GitHub Pages plugin here
-  ],
-  base: '/Talentime/', // Replace 'Talentime' with your repository name
-});
+  plugins: [react()],
+  base: '/talentime/',
+})
